232f577c151670004c1ff30676815cf9aa79c2a5
theo.fabien@inrae.fr <theo.fabien@inrae.fr>
2025-07-17 22:32:26 +0200

    Second commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('resume-button').addEventListener('click', function() {
+    window.location.href = 'https://www.google.com'; // Replace with the URL you want to navigate to
+});
